5e0a40188521ce51a8e1dda82d591964cda34846
root <root@ip-172-31-43-92.sa-east-1.compute.internal>
2026-06-18 11:07:49 +0000

    This is my second commit

--- facebook.java
+++ facebook.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;  
+This is my git session...!
 import java.lang.Math;  
 import java.io.printer; 
 import java.io.collection; 
@@ -24,6 +24,18 @@ System.out.print(Math.max(Math.abs(i - n), Math.abs(j - n)) + 1 + " ");
 System.out.println();  
 }  
 }  
+//driver code  
+public static void main(String args[])  
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the value of n: ");  
+int n = sc.nextInt();  
+System.out.println();  
+//function calling  
+printPattern(n);  
+}  
+}
+
 //driver code  
 public static void main(String args[])  
 {  
